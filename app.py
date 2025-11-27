3eab834aaeb9412956f3e379ce10a2c26ca6c49b
Martin <martin.jobard@telecomnancy.eu>
2025-11-27 14:34:44 +0100

    debut de la structure du site

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from jinja2 import Environment, FileSystemLoader
 from flask import Flask, render_template
 app = Flask(__name__)
-
+"""
 file_loader = FileSystemLoader("./template")
 
 env = Environment(loader = file_loader)
@@ -10,10 +10,52 @@ template = env.get_template("test.html")
 
 html = template.render(h1="Tableau de bord",
                        title="Telecom Nancy Service") #Nom du site et contenu principal
-
+"""
 @app.route('/')
-def hello_world():
+def Accueil():
+    titre_site = "Site interne TNS"
+    return render_template('Accueil.html',titre = titre_site)
+
+@app.route('/Clients')
+def Clients():
     return 'Hello, World!'
 
+@app.route('/Projets')
+def Projets():
+    return 'Hello, World!'
 
-print(html)
+@app.route('/Intervenants')
+def Intervenants():
+    return 'Hello, World!'
+
+@app.route('/Import-Export')
+def Import_Export():
+    return 'Hello, World!'
+
+@app.route('/Wiki-Docs')
+def Wiki_Docs():
+    return 'Hello, World!'
+
+@app.route('/tinder_like')
+def tinder_like():
+    return 'Hello, World!'
+
+@app.route('/Stats')
+def Stats():
+    return 'Hello, World!'
+
+@app.route('/Missions_réalisées')
+def Missions_réalisées():
+    return 'Hello, World!'
+
+@app.route('/Partenaires')
+def Partenaires():
+    return 'Hello, World!'
+
+@app.route('/Contact')
+def Contact():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
