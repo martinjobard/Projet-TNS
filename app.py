da62b293aa2bad3f33a411658cdd13c7bfd48785
Anaelle <anaelle.jeckert@telecomnancy.eu>
2025-12-03 16:49:54 +0100

    ajout de error intervenants et modif app.py

--- app.py
+++ app.py
@@ -237,7 +237,7 @@ def Inter_profil(nomcomplet=None):
     db = get_db()
     c = db.cursor()
     [name, surname]=nomcomplet.split('.')
-    sql = "SELECT Nom, Prénom FROM Intervenants WHERE nom=? AND prénom=?"
+    sql = "SELECT nom, prénom FROM Clients nom=? AND prénom=?"
     c.execute(sql, (name, surname))
     selected_students = c.fetchone() #plus trouver les compétences 
     if selected_students is None:
